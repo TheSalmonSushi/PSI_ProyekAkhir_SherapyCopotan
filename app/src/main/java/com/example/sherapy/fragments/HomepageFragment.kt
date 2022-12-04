f1ebc6fe7c8d31570f294452910cdb03f8a84cf4
AlexanderRama <90639823+AlexanderRama@users.noreply.github.com>
2022-12-04 20:49:19 +0700

    fx

--- app/src/main/java/com/example/sherapy/fragments/HomepageFragment.kt
+++ app/src/main/java/com/example/sherapy/fragments/HomepageFragment.kt
@@ -3,10 +3,12 @@ package com.example.sherapy.fragments
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.sherapy.R
@@ -16,12 +18,16 @@ import com.example.sherapy.utilities.Constants
 import com.example.sherapy.utilities.NewsAdapter
 import com.example.sherapy.utilities.OnNewsClickListener
 import com.example.sherapy.utilities.PreferenceManager
+import com.kwabenaberko.newsapilib.NewsApiClient
 import com.kwabenaberko.newsapilib.models.Article
+import com.kwabenaberko.newsapilib.models.request.EverythingRequest
+import com.kwabenaberko.newsapilib.models.response.ArticleResponse
 
 class HomepageFragment : Fragment() {
 
     private lateinit var binding: FragmentHomepageBinding
     private lateinit var preferenceManager: PreferenceManager
+    private val newsApiClient = NewsApiClient("abdd96cad11947489f4a80ae0bb9a1a3")
     val newsItem = arrayListOf<Article>()
 
     override fun onCreateView(
@@ -54,6 +60,30 @@ class HomepageFragment : Fragment() {
 //        val bytes: ByteArray = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT)
 //        val bitmap: Bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
 //        binding.imageProfile.setImageBitmap(bitmap)
+    }
+
+    private fun fetchNews() {
+        newsApiClient.getEverything(
+            EverythingRequest.Builder()
+                .q("Mental Health OR Mental Health Issue OR Mental OR Mental health")
+                .language("en")
+                .build(),
+            object: NewsApiClient.ArticlesResponseCallback {
+                override fun onSuccess(response: ArticleResponse?) {
+                    response?.articles?.forEach {
+                        newsItem.add(it)
+                    }
+                    bindNews()
+                }
+
+                override fun onFailure(throwable: Throwable?) {
+                    throwable?.message?.let {
+                        Log.e("Error", it)
+                        Toast.makeText(requireContext(), it, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        )
     }
 
     private fun bindNews() {
